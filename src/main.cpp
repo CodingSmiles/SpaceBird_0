26627a227d6ef06f8d9763e4cb95a0db3a938132
CodingSmiles <aadirajanil726@gmail.com>
2022-05-18 17:38:43 +0530

    big changes

--- src/main.cpp
+++ src/main.cpp
@@ -14,31 +14,80 @@
 */
 
 #include <Arduino.h>
-#include <TimerOne.h>
+#include <TimerThree.h>
 
-int outPin = 9; // Connect the transistor's base to pin 9
+#define outPin 9            // Connect the transistor's base to pin 9
+#define igniterPin 10       // Connect to igniter transistor's base pin
+#define trigPin 10          // Fuel Level Sensor Trigger Pin
+#define echoPin 13          // Fuel Level Sensor Echo Pin
+double duration, fuelLevel; // Fuel Level Values
+bool pumpState = true;      // true will speed up the pumps, false will do the opposite
 
-// Variable to hold pump speed value
-volatile unsigned long speedVal = 0;
+volatile unsigned long speedVal = 0;     // Variable to hold pump speed value
+volatile unsigned long incrementVal = 1; // Variable to hold pump speed increment value
+volatile unsigned long decrementVal = 1; // Variable to hold pump speed decrement value
 
 void setup()
 {
   Serial.begin(115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   pinMode(outPin, OUTPUT);
-  Timer1.initialize(150000);
-  Timer1.attachInterrupt(increment);
+  pinMode(igniterPin, OUTPUT);
+  Timer3.initialize(150000);
+  Timer3.attachInterrupt(increment);
 }
 
 void increment()
 {
-  while (speedVal < 255)
+  if (pumpState == true)
   {
-    speedVal++; // Each increment will increase the fuel flow by approx 7.8ml per minute
+    if (speedVal < 255)
+    {
+      speedVal + incrementVal; // Each increment will increase the fuel flow by approx 7.8ml per minute
+    }
+    else
+    {
+    }
   }
+  else
+  {
+    speedVal - decrementVal;
+  }
+}
+
+double getFuelLevel()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  fuelLevel = (duration / 2) * 0.0343;
+  return fuelLevel;
+}
+
+void parachute()
+{
+  pumpState = false;
+
+  analogWrite(outPin, 0);
+  digitalWrite(igniterPin, LOW);
 }
 
 void loop()
 {
+  getFuelLevel();
+  if (fuelLevel < 15.0)
+  {
+    pumpState = false;
+    decrementVal = 1;
+    digitalWrite(igniterPin, HIGH);
+    analogWrite(outPin, speedVal);
+  }
+  // Start Igniter
+  digitalWrite(igniterPin, HIGH);
   // Write PWM to transistor
   analogWrite(outPin, speedVal);
 }
